e13f5fa5a100c7b395242cf8d87a0c9fbe1cab8e
unknown <junejungjunejung@gmail.com>
2020-11-17 23:11:02 -0800

    mobile hamburger menu, about content

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -3,7 +3,57 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div id="about-layout">
-      i'm the about page
+      <div>
+        <h1>Web and Mobile UX Designer & Front-End developer.</h1>
+        <p>I'm a ... based in Vancouver, Canada.</p>
+        <p>Let's work together.</p>
+      </div>
+
+      <div>
+        <h1>Tools & Skills</h1>
+
+        <div>
+          <h2>Design</h2>
+
+          <div>
+            <h3>Creative Process</h3>
+
+            <p>User Flow</p>
+            <p>Wireframe</p>
+            <p>Prototype</p>
+            <p>Mockup</p>
+          </div>
+
+          <div>
+            <h3>Softwares</h3>
+
+            <p>Figma</p>
+            <p>Zeplin</p>
+            <p>Adobe Photoshop</p>
+            <p>Adobe Illustrator</p>
+            <p>Adobe XD</p>
+            <p>Adobe InDesign</p>
+            <p>Trello</p>
+          </div>
+        </div>
+
+        <div>
+          <h2>Development</h2>
+
+          <p>React.JS</p>
+          <p>Rest API</p>
+          <p>Firebase</p>
+          <p>Node.Js</p>
+          <p>Git</p>
+          <p>NPM</p>
+          <p>Yarn</p>
+          <p>Webpack</p>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>JavaScript</p>
+          <p>SCSS</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
 import { GitHub, LinkedIn } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,9 +18,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(!open);
+  };
 
   return (
+    <div>
       <Toolbar id="header-layout" className={classes.toolbar}>
+
+        <IconButton
+          id="nav-hamburger-button"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer(open, true)}
+          edge="start"
+        >
+          <MenuIcon />
+        </IconButton>
+
         <ul id="nav-menu-layout">
           <li>
             <Link to="/">Projects</Link>
@@ -28,11 +53,11 @@ const Header = () => {
           </li>
         </ul>
 
-        <div>
+        <div id="june-jung">
           <Link to="/">June Jung</Link>
         </div>
 
-        <div>
+        <div id="nav-social">
           <a href="https://www.linkedin.com/in/junejungjunejung/" className="Social-links" target="_blank" rel="noopener noreferrer">
             <IconButton color="inherit">
               <LinkedIn />
@@ -46,6 +71,51 @@ const Header = () => {
           </a>
         </div>
       </Toolbar>
+
+      <Drawer
+        anchor="left"
+        open={open}
+      >
+        <div id="drawer-close-bttn">
+          <IconButton onClick={toggleDrawer(open, false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+
+        <div id="drawer-layout">
+          <ul
+            id="nav-hamburger"
+            onClick={toggleDrawer(open, false)}
+            onKeyDown={toggleDrawer(open, false)}
+            >
+            <li>
+              <Link to="/">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <div id="nav-social-hamburger">
+            <a href="https://www.linkedin.com/in/junejungjunejung/" className="Social-links" target="_blank" rel="noopener noreferrer">
+              <IconButton color="inherit">
+                <LinkedIn />
+              </IconButton>
+            </a>
+
+            <a href="https://github.com/junejungjunejung" className="Social-links" target="_blank" rel="noopener noreferrer">
+              <IconButton color="inherit">
+                <GitHub />
+              </IconButton>
+            </a>
+          </div>
+        </div>
+      </Drawer>
+    </div>
   );
 }
 
