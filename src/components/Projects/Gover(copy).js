5934103f7a894b13f47a1c7fd5a996fe9b5a435f
unknown <junejungjunejung@gmail.com>
2020-12-17 23:57:12 -0800

    edit gover project content

--- src/components/Projects/Gover(copy).js
+++ src/components/Projects/Gover(copy).js
@@ -2,29 +2,55 @@ import React from 'react';
 import Divider from '@material-ui/core/Divider';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
-import competitors from '../../image/competitors.jpg';
 import howitworks from '../../image/gover-howitworks.jpg';
 import userflow from '../../image/gover-userflow.jpg';
+
 
 import landingWeb from '../../image/gover-landing-web-wire.jpg';
 import dashboardWeb from '../../image/gover-dashboard-web-wire.jpg';
 import reportsWeb from '../../image/gover-reports-web-wire.jpg';
 import reportItemWeb from '../../image/gover-report-item-web-wire.jpg';
 
+import landingMobile from '../../image/gover-landing-mob-wire.jpg';
+import dashboardMobile from '../../image/gover-dashboard-mob-wire.jpg';
+import reportsMobile from '../../image/gover-reports-mob-wire.jpg';
+import reportItemtMobile from '../../image/gover-report-item-mob-wire.jpg';
+
+
 import homeWeb from '../../image/gover-home-web-wire.jpg';
 import priceWeb from '../../image/gover-price-web-wire.jpg';
 import aboutWeb from '../../image/gover-about-web-wire.jpg';
 import contactWeb from '../../image/gover-contact-web-wire.jpg';
+
+import homeMobile from '../../image/gover-home-mob-wire.jpg';
+import menuMobile from '../../image/gover-menu-mob-wire.jpg';
+import priceMobile from '../../image/gover-price-mob-wire.jpg';
+import aboutMobile from '../../image/gover-about-mob-wire.jpg';
+import contactMobile from '../../image/gover-contact-mob-wire.jpg';
+
 
 import landingWebMock from '../../image/gover-landing-web-mock.jpg';
 import dashboardWebMock from '../../image/gover-dashboard-web-mock.jpg';
 import reportsWebMock from '../../image/gover-reports-web-mock.jpg';
 import reportItemWebMock from '../../image/gover-report-item-web-mock.jpg';
 
+import landingMobileMock from '../../image/gover-landing-mob-mock.jpg';
+import dashboardMobileMock from '../../image/gover-dashboard-mob-mock.jpg';
+import reportsMobileMock from '../../image/gover-reports-mob-mock.jpg';
+import reportItemtMobileMock from '../../image/gover-report-item-mob-mock.jpg';
+
+
 import homeWebMoc from '../../image/gover-home-web-mock.jpg';
 import priceWebMoc from '../../image/gover-price-web-mock.jpg';
 import aboutWebMoc from '../../image/gover-about-web-mock.jpg';
 import contactWebMoc from '../../image/gover-contact-web-mock.jpg';
+
+import homeMobileMoc from '../../image/gover-home-mob-mock.jpg';
+import menuMobileMoc from '../../image/gover-menu-mob-mock.jpg';
+import priceMobileMoc from '../../image/gover-price-mob-mock.jpg';
+import aboutMobileMoc from '../../image/gover-about-mob-mock.jpg';
+import contactMobileMoc from '../../image/gover-contact-mob-mock.jpg';
+
 
 import assets from '../../image/gover-assets.jpg';
 import color from '../../image/gover-color.jpg';
@@ -43,7 +69,7 @@ const Gover = () => {
         <h2>Multiplatform Service Design & Development Project</h2>
         <p>Gover is a multiplatform service that enables property and equipment inspection seamless and faster.</p>
         <p>There are three parts to the project that are the native Android app, the web app, and the corporate website. I took part in designing and developing mainly the web app and the corporate website.</p>
-      </div>
+        </div>
 
       <div className="textbox">
         <h2>My role</h2>
@@ -67,16 +93,11 @@ const Gover = () => {
 
       <Divider className="divider"/>
 
-      <div className="textbox">
-        <div>
+      <div>
+        <div className="textbox">
             <h2>UX Design</h2>
             <p>There are many property inspection apps available out there. However, most of these apps are catering to big property management firms, with complicated inspection customization. Gover is offering a simple and affordable solution for small scale property and equipment owners.
             </p>
-
-            <div>
-              <h3>Competitive Landscape</h3>
-              <img src={competitors} alt=""/>
-            </div> 
 
             <h3>Personas</h3>
             <p>I created three personas with different goals and frustrations. To cater to the users with different backgrounds and needs, I decided to provide inspection templates based on their chosen property type.</p>
@@ -185,11 +206,11 @@ const Gover = () => {
 
           <Divider className="divider"/>
           <div>
-            <h3>How Gover works</h3>
+            <h3 className="textbox">How Gover works</h3>
             <img src={howitworks} alt=""/>
           </div> 
 
-          <h3>Userflow</h3>
+          <h3 className="textbox">Userflow</h3>
           <div className="textbox">
             <p>The user flow below shows that the corporate website is connected to the web app via the login button. The web app can be only accessible by users with credentials. The web app provides the same functionality as the Android app except for the native mobile behavior such as the camera roll and the voice to text access.</p>
           </div>
@@ -198,54 +219,103 @@ const Gover = () => {
           <Divider className="divider"/>
 
           <div>
-            <h3>Wireframe - Web App</h3>
+            <h3 className="textbox">Wireframe - Web App</h3>
             <div>
               <div>
                 <div className="web-wireframe">
                   <div>
-                    <h3>Landing page desktop wireframe</h3>
+                    <h3 className="textbox">Landing page desktop wireframe</h3>
                     <p className="textbox"> Only the signed-in users can access to Gover's main service.</p>
                     <img src={landingWeb} alt="desktop wireframe"/>
                   </div>
         
                   <div>
-                    <h3>Dashboard desktop wireframe</h3>
+                    <h3 className="textbox">Dashboard desktop wireframe</h3>
                     <img src={dashboardWeb} alt="desktop wireframe"/>
                   </div>
 
                   <div>
-                    <h3>Reports desktop wireframe</h3>
+                    <h3 className="textbox">Reports desktop wireframe</h3>
                     <img src={reportsWeb} alt="desktop wireframe"/>
                   </div>
 
                   <div>
-                    <h3>Report item desktop wireframe</h3>
+                    <h3 className="textbox">Report item desktop wireframe</h3>
                     <img src={reportItemWeb} alt="desktop wireframe"/>
+                  </div>
+                </div>
+        
+                <div className="mobile-wireframe">
+                  <div>
+                    <h3>Landing page mobile wireframe</h3>
+                    <img src={landingMobile} alt="mobile wireframe"/>
+                  </div> 
+
+                  <div>
+                    <h3>Dashboard mobile wireframe</h3>
+                    <img src={dashboardMobile} alt="mobile wireframe"/>
+                  </div>
+
+                  <div>
+                    <h3>Reports mobile wireframe</h3>
+                    <img src={reportsMobile} alt="mobile wireframe"/>
+                  </div>
+                  
+                  <div>
+                    <h3>Report Item mobile wireframe</h3>
+                    <img src={reportItemtMobile} alt="mobile wireframe"/>
                   </div>
                 </div>
               </div>
 
               <div>
-                <h3>Wireframe - Corporate website</h3>
+                <h3 className="textbox">Wireframe - Corporate website</h3>
                 <div className="web-wireframe">
                   <div>
-                    <h3>Landing page desktop wireframe</h3>
+                    <h3 className="textbox">Landing page desktop wireframe</h3>
                     <img src={homeWeb} alt="desktop wireframe"/>
                   </div>
 
                   <div>
-                    <h3>Pricing page desktop wireframe</h3>
+                    <h3 className="textbox">Pricing page desktop wireframe</h3>
                     <img src={priceWeb} alt="desktop wireframe"/>
                   </div>                  
 
                   <div>
-                    <h3>About page desktop wireframe</h3>
+                    <h3 className="textbox">About page desktop wireframe</h3>
                     <img src={aboutWeb} alt="desktop wireframe"/>
                   </div>
 
                   <div>
-                    <h3> Contact page desktop wireframe</h3>
+                    <h3 className="textbox"> Contact page desktop wireframe</h3>
                     <img src={contactWeb} alt="desktop wireframe"/>
+                  </div>
+                </div>
+
+                <div className="mobile-wireframe">
+                  <div>
+                    <h3>Landing page mobile wireframe</h3>
+                    <img src={homeMobile} alt="mobile wireframe"/>
+                  </div>
+        
+                  <div>
+                    <h3>Mobile navigation wireframe</h3>
+                    <img src={menuMobile} alt="mobile wireframe"/>
+                  </div>
+        
+                  <div>
+                    <h3>Pricing page mobile wireframe</h3>
+                    <img src={priceMobile} alt="mobile wireframe"/>
+                  </div> 
+
+                  <div>
+                    <h3>About page mobile wireframe</h3>
+                    <img src={aboutMobile} alt="mobile wireframe"/>
+                  </div>
+        
+                  <div>
+                    <h3>Contact page mobile wireframe</h3>
+                    <img src={contactMobile} alt="mobile wireframe"/>
                   </div>
                 </div>
               </div>
@@ -254,52 +324,101 @@ const Gover = () => {
             <Divider className="divider"/>
 
             <div>
-              <h3>Mockup - Web App</h3>
+              <h3 className="textbox">Mockup - Web App</h3>
               <div>
                 <div className="web-mockup">
                   <div>
-                    <h3>Landing page desktop mockup</h3>
+                    <h3 className="textbox">Landing page desktop mockup</h3>
                     <img src={landingWebMock} alt="desktop mockup"/>
                   </div>
         
                   <div>
-                    <h3>Dashboard desktop mockup</h3>
+                    <h3 className="textbox">Dashboard desktop mockup</h3>
                     <img src={dashboardWebMock} alt="desktop mockup"/>
                   </div>
 
                   <div>
-                    <h3>Reports desktop mockup</h3>
+                    <h3 className="textbox">Reports desktop mockup</h3>
                     <img src={reportsWebMock} alt="desktop mockup"/>
                   </div>
 
                   <div>
-                    <h3>Report item desktop mockup</h3>
+                    <h3 className="textbox">Report item desktop mockup</h3>
                     <img src={reportItemWebMock} alt="desktop mockup"/>
+                  </div>
+                </div>
+        
+                <div className="mobile-mockup">
+                  <div>
+                    <h3>Landing page mobile mockup</h3>
+                    <img src={landingMobileMock} alt="mobile mockup"/>
+                  </div>
+        
+                  <div>
+                    <h3>Dashboard mobile mockup</h3>
+                    <img src={dashboardMobileMock} alt="mobile mockup"/>
+                  </div>
+        
+                  <div>
+                    <h3>Reports mobile mockup</h3>
+                    <img src={reportsMobileMock} alt="mobile mockup"/>
+                  </div>
+        
+                  <div>
+                    <h3>Report Item mobile mockup</h3>
+                    <img src={reportItemtMobileMock} alt="mobile mockup"/>
                   </div>
                 </div>
               </div>
 
               <div>
-                <h3>Mockup - Corporate website</h3>
+                <h3 className="textbox">Mockup - Corporate website</h3>
                 <div className="web-mockup">
                   <div>
-                    <h3>Landing page desktop mockup</h3>
+                    <h3 className="textbox">Landing page desktop mockup</h3>
                     <img src={homeWebMoc} alt="desktop mockup"/>
                   </div>
 
                   <div>
-                    <h3>Pricing page desktop mockup</h3>
+                    <h3 className="textbox">Pricing page desktop mockup</h3>
                     <img src={priceWebMoc} alt="desktop mockup"/>
                   </div>                  
 
                   <div>
-                    <h3>About page desktop mockup</h3>
+                    <h3 className="textbox">About page desktop mockup</h3>
                     <img src={aboutWebMoc} alt="desktop mockup"/>
                   </div>
 
                   <div>
-                    <h3>Contact page desktop mockup</h3>
+                    <h3 className="textbox">Contact page desktop mockup</h3>
                     <img src={contactWebMoc} alt="desktop mockup"/>
+                  </div>
+                </div>
+
+                <div className="mobile-mockup">
+                  <div>
+                    <h3>Landing page mobile mockup</h3>
+                    <img src={homeMobileMoc} alt="mobile mockup"/>
+                  </div>
+        
+                  <div>
+                    <h3>Mobile navigation mockup</h3>
+                    <img src={menuMobileMoc} alt="mobile mockup"/>
+                  </div>
+
+                  <div>
+                    <h3>Pricing page mobile mockup</h3>
+                    <img src={priceMobileMoc} alt="mobile mockup"/>
+                  </div>                  
+
+                  <div>
+                    <h3>About page mobile mockup</h3>
+                    <img src={aboutMobileMoc} alt="mobile mockup"/>
+                  </div>
+        
+                  <div>
+                    <h3>Contact page mobile mockup</h3>
+                    <img src={contactMobileMoc} alt="mobile mockup"/>
                   </div>
                 </div>
               </div>
